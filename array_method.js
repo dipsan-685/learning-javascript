c38854ba705781163fc7d8d89c4c47e5bb0921d5
dipsan-685 <dipsanradiant@gmail.com>
2026-02-11 20:08:55 +0545

    methods of array

--- array_method.js
+++ array_method.js
@@ -7,20 +7,25 @@ console.log(arr);
 // arr.pop()
 // console.log(arr);
 
-arr.unshift(0);
-console.log(arr);
-
-arr.shift();
-console.log(arr);
-
-arr.splice(1,1);
-console.log(arr);
-
-// arr.splice(1,0,5);
+// arr.unshift(0);
 // console.log(arr);
 
-// arr.splice(2,0,6,7);
+// arr.shift();
 // console.log(arr);
 
-// arr.splice(2,2);
-// console.log(arr);       
+// arr.splice(1,1);
+// console.log(arr);
+
+arr.reverse();
+console.log(arr);
+
+arr.sort();
+console.log(arr);
+
+arr = arr.join("-");
+console.log(arr);   
+
+
+
+
+     
