acd9171817302674b0898d40ec499448f59fc90f
dipsan-685 <dipsanradiant@gmail.com>
2026-02-11 19:53:49 +0545

    array methods example

---
+++ array_method.js
@@ -0,0 +1,26 @@
+let arr =[1,2,3];
+console.log(arr);
+
+// arr.push(1000)
+// console.log(arr);
+
+// arr.pop()
+// console.log(arr);
+
+arr.unshift(0);
+console.log(arr);
+
+arr.shift();
+console.log(arr);
+
+arr.splice(1,1);
+console.log(arr);
+
+// arr.splice(1,0,5);
+// console.log(arr);
+
+// arr.splice(2,0,6,7);
+// console.log(arr);
+
+// arr.splice(2,2);
+// console.log(arr);       
