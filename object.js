207a0044df03f9e33e597a1206a4d76b1c4276f9
dipsan-685 <dipsanradiant@gmail.com>
2026-02-12 19:10:40 +0545

    object with function

--- object.js
+++ object.js
@@ -3,11 +3,17 @@ const studentdata = {
     age: 20,
     address: "manigram",
     isgraduated: false,
+    changename: function(name){
+        this.name = name;
+    }
 
 };
 
+console.log(studentdata.name); // entire object
 // Accessing properties
-console.log(studentdata.name); // dipsan
-console.log(studentdata.age); // 18
+console.log(studentdata.age); // 20
 console.log(studentdata.address); // manigram
 console.log(studentdata.isgraduated); // false
+
+studentdata.changename("dipsan");
+console.log(studentdata.name);  
