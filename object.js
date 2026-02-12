d4bf40ac5e04c46fbe76838fd2ab3d8838496e11
dipsan-685 <dipsanradiant@gmail.com>
2026-02-12 19:26:34 +0545

    employee detail updated

--- object.js
+++ object.js
@@ -3,7 +3,7 @@ const studentdata = {
     age: 20,
     address: "manigram",
     isgraduated: false,
-    changename: function(name){
+    changename: function (name) {
         this.name = name;
     },
 
@@ -13,8 +13,8 @@ const studentdata = {
         english: 92
 
     },
-    workdays: [1,2,3,4,5]  
-    
+    workdays: [1, 2, 3, 4, 5]
+
 
 };
 
@@ -27,19 +27,21 @@ console.log(studentdata.marks.math); // 90
 console.log(studentdata.workdays[2]); // 3
 
 studentdata.changename("dipsan");
-console.log(studentdata.name);  
+console.log(studentdata.name);
 
 
 // emploey details
 
 const employeedata = {
     name: "susan",
+    id: 1001,
     age: 30,
     department: "software development",
     isfulltime: false,
 }
 
 console.log(employeedata.name); // susan
+console.log(employeedata.id); // 1001   
 console.log(employeedata.age); // 30
-console.log(employeedata.department); // HR
-console.log(employeedata.isfulltime); // true
+console.log(employeedata.department); // software development
+console.log(employeedata.isfulltime); // false
