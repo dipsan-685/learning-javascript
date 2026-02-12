48cc11b523595df596115c9055add5c5210753a1
dipsan-685 <dipsanradiant@gmail.com>
2026-02-12 19:19:16 +0545

    nested object

--- object.js
+++ object.js
@@ -5,7 +5,16 @@ const studentdata = {
     isgraduated: false,
     changename: function(name){
         this.name = name;
-    }
+    },
+
+    marks: {
+        math: 90,
+        science: 85,
+        english: 92
+
+    },
+    workdays: [1,2,3,4,5]  
+    
 
 };
 
@@ -14,6 +23,8 @@ console.log(studentdata.name); // entire object
 console.log(studentdata.age); // 20
 console.log(studentdata.address); // manigram
 console.log(studentdata.isgraduated); // false
+console.log(studentdata.marks.math); // 90
+console.log(studentdata.workdays[2]); // 3
 
 studentdata.changename("dipsan");
 console.log(studentdata.name);  
