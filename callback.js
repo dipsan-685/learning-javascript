9c10f9d7470383fdc8c925c4f013a01fa9e5cd22
dipsan-685 <dipsanradiant@gmail.com>
2026-02-16 18:43:38 +0545

    new call back function

--- callback.js
+++ callback.js
@@ -1,5 +1,17 @@
 // callback function pass argument to another function so it called call back function
 
+// normal function 
+function a() {
+    console.log('I am a function');
+}
+
+
+//arrow function 
+const b = () => {
+    console.log('I am an arrow function');
+}
+
+// callback function
 function greet(name) {
     console.log('Hello ' + name);
 }
